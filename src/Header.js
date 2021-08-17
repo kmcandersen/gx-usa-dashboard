c483c8b9587d872b0ae5b8f5eef3a77b2ed508e8
kmcandersen <kristen.andersen@gmail.com>
2021-08-16 21:49:57 -0500

    set up react

---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import logo from './gx-sign-blue.svg';
+
+const Header = () => {
+    return (
+        <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          rrgxdb
+        </p>
+      </header>
+    )
+}
+
+export default Header
