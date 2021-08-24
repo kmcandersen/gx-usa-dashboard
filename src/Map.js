c62b73d694c0a78eab138e23dd3e08c15cd90e8e
kmcandersen <kristen.andersen@gmail.com>
2021-08-23 21:05:44 -0500

    set up Map

--- src/Map.js
+++ src/Map.js
@@ -1,12 +1,115 @@
-import React, { useEffect } from 'react';
+import React, { useContext, useEffect, useRef, useState } from 'react';
+import {
+  extent,
+  format,
+  interpolateBlues,
+  max,
+  min,
+  scaleSequentialPow,
+  select,
+} from 'd3';
+import DataContext from './context/DataContext';
+import { ReactComponent as UsMap } from './assets/us_states_map.svg';
 import './App.css';
 
 const Map = ({ mapDimensions }) => {
-  useEffect(() => {
-    // console.log(mapDimensions);
-  }, [mapDimensions]);
+  const { stateTotData } = useContext(DataContext);
+  //const [svgDimensions, setSvgDimensions] = useState();
 
-  return <div>MAP</div>;
+  const svgRef = useRef();
+
+  useEffect(() => {
+    if (stateTotData) {
+      const metricAccessor = (d) => d.TOTINC;
+
+      const colorScale = scaleSequentialPow(interpolateBlues)
+        // low density = lighter
+        .domain([0, max(stateTotData, metricAccessor)])
+        // .domain(extent(stateTotData, metricAccessor))
+        .range(['#00d4ff', '#090979'])
+        // > 1 = flatter/less variation; < 1 = more variation in colors
+        .exponent(0.3);
+      // can also base color scale on min, max data values
+
+      const svg = select(svgRef.current);
+      svg
+        .attr('viewBox', `0 0 618 380`)
+        .attr('preserveAspectRatio', 'xMaxYMax meet')
+        .attr('width', '100%')
+        .attr('height', '100%');
+
+      const usStates = svg
+        .selectAll('path')
+        .data(stateTotData)
+        .join('path.border')
+        .attr('data-value', metricAccessor)
+        .attr('fill', (d) => {
+          // if (d.TOTINC > 100) {
+          //   return 'blue';
+          // } else {
+          //   return 'green';
+          // }
+          return colorScale(d.TOTINC);
+        });
+
+      function getBoundingBoxCenter(element) {
+        var bbox = element.getBBox();
+        // return the center of the bounding box
+        return [bbox.x + bbox.width / 2, bbox.y + bbox.height / 2];
+      }
+
+      svg.on('mouseover', (e) => {
+        // tooltip removed if mouse moves outside state paths
+        if (e.target.tagName === 'svg') {
+          svg.select('g#state-tooltip').remove();
+        }
+        if (e.target.tagName === 'path') {
+          svg.select('g#state-tooltip').remove();
+          const hoveredState = e.target;
+          const bbCtr = getBoundingBoxCenter(hoveredState);
+          const stateTooltip = svg.append('g').attr('id', 'state-tooltip');
+
+          stateTooltip
+            .append('rect')
+            .attr('x', bbCtr[0] - 35)
+            .attr('y', bbCtr[1] - 12.5)
+            .attr('height', 25)
+            .attr('width', 70)
+            .style('opacity', 0)
+            .transition()
+            .duration(250)
+            .style('opacity', 1);
+          stateTooltip
+            .append('text')
+            .attr('x', bbCtr[0])
+            .attr('y', bbCtr[1])
+            .text(
+              `${hoveredState.id}: ${Number(
+                hoveredState.dataset.value
+              ).toLocaleString()}`
+            )
+            .style('opacity', 0)
+            .transition()
+            .duration(250)
+            .style('opacity', 1);
+        }
+      });
+    }
+  }, [mapDimensions, stateTotData]);
+  if (mapDimensions && stateTotData) {
+    return (
+      <div
+        style={{
+          width: mapDimensions.width * 0.9,
+          height: mapDimensions.width * 0.9 * 0.62,
+        }}
+      >
+        <UsMap ref={svgRef} />
+        <div id='legend'></div>
+      </div>
+    );
+  }
+  return null;
 };
 
 export default Map;
--- src/context/DataContext.js
+++ src/context/DataContext.js
@@ -1,12 +1,21 @@
 import React, { useEffect, useState } from 'react';
 import { csv } from 'd3';
 import usDataCsv from './../assets/inctotus_16_20.csv';
+import stDataCsv from './../assets/incbystate_16_20.csv';
+import stTotIncCsv from './../assets/inctotbystate_16_20.csv';
+//import usStateGeo from './../assets/gz_2010_us_040_00_20m.json';
 
 const DataContext = React.createContext();
 
 export const DataProvider = ({ children }) => {
   const [usData, setUSData] = useState();
+  const [stateYrData, setStateYrData] = useState();
+  const [stateTotData, setStateTotData] = useState();
+  // const [stateGeo, setStateGeo] = useState();
   const [error, setError] = useState(null);
+
+  // console.log('StateCsv', usDataCsv);
+  // console.log('usStateGeo', usStateGeo);
 
   const getUsData = async () => {
     try {
@@ -23,12 +32,61 @@ export const DataProvider = ({ children }) => {
     }
   };
 
-  useEffect(() => getUsData(), []);
+  const getStateYrData = async () => {
+    try {
+      setError(null);
+      const dataset = await csv(stDataCsv);
+      dataset.forEach((d) => {
+        d.YEAR = +d.YEAR;
+        d.EQFREIGHT = +d.EQFREIGHT;
+        d.EQPSGR = +d.EQPSGR;
+        d.EQYDMAINT = +d.EQYDMAINT;
+        d.EQOTHER = +d.EQOTHER;
+        d.VEHAUTO = +d.VEHAUTO;
+        d.VEHPED = +d.VEHPED;
+        d.VEHTRUCK = +d.VEHTRUCK;
+        d.VEHOTHER = +d.VEHOTHER;
+        d.TOTINC = +d.TOTINC;
+        d.TOTINJ = +d.TOTINJ;
+        d.TOTKLD = +d.TOTKLD;
+      });
+      setStateYrData(dataset);
+    } catch (error) {
+      setError(error.toString());
+    }
+  };
+
+  const getStateTotData = async () => {
+    try {
+      setError(null);
+      const dataset = await csv(stTotIncCsv);
+
+      dataset.forEach((d) => {
+        d.TOTINC = +d.TOTINC;
+      });
+      setStateTotData(dataset);
+    } catch (error) {
+      setError(error.toString());
+    }
+  };
+
+  // const getStateGeo = async () => {
+  //   setStateGeo(usStateGeo);
+  // };
+
+  useEffect(() => {
+    getUsData();
+    getStateYrData();
+    getStateTotData();
+  }, []);
 
   return (
     <DataContext.Provider
       value={{
         usData,
+        stateYrData,
+        stateTotData,
+        // stateGeo,
         error,
       }}
     >
