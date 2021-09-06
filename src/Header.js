e977c33ef085f3e34e3cac99de30d9d927bf4c1e
kmcandersen <kristen.andersen@gmail.com>
2021-09-05 20:51:49 -0500

    revise colors & style header

--- src/Header.js
+++ src/Header.js
@@ -1,16 +1,35 @@
 import React from 'react';
 import './App.css';
-import logo from './assets/gx-sign-blue.svg';
+import { ReactComponent as Logo } from './assets/gx-sign-blue.svg';
 
 const Header = () => {
   return (
     <header className='header-wrapper'>
+      {/* <div style={{ display: 'flex' }}> */}
       <div className='header-logo'>
-        <img src={logo} alt='logo' />
-        <p>rrgxdb</p>
-      </div>
-      <div className='header-name'>
-        <p>railroad grade crossing dashboard</p>
+        <div className='header-title-group'>
+          <div className='header-title-logo'>
+            <Logo
+              width={45}
+              height={40}
+              viewBox={`0 -5 118 118`}
+              style={{ transform: `translateY(5px)` }}
+            />
+            <p className='header-title'>
+              rrgxdb<span className='header-year'> 2000-2020</span>
+            </p>
+          </div>
+          <p className='header-subtitle'>railroad grade crossing dashboard</p>
+        </div>
+
+        <p className='header-desc subtitle'>
+          Collisions at highway-rail at-grade crossings in the United States.
+          <br />
+          Source:{' '}
+          <a href='https://safetydata.fra.dot.gov/'>
+            Federal Railroad Administration
+          </a>
+        </p>
       </div>
     </header>
   );
